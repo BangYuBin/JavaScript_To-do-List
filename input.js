c516730a408bf0b958de069ff35d468f25585b14
YuBin <ttang1135@naver.com>
2020-03-29 17:45:22 +0900

    삭제 버튼, table CSS 수정

--- input.js
+++ input.js
@@ -12,13 +12,13 @@ function add_task(){
     //체크박스 생성
     let input = document.createElement('input');
     input.setAttribute('type', 'checkbox');
-    input.setAttribute('class', 'btn-chk');
+    input.setAttribute('class', 'btn-chk custom-control-input');
     input.addEventListener('click', fun_checked);
 
     //삭제 버튼 생성    
     let remove = document.createElement('button');
     let remove_Text = document.createTextNode("삭제");
-    remove.setAttribute('class', 'btn-rem');
+    remove.setAttribute('class', 'btn-rem btn btn-danger');
     //remove.setAttribute('onclick', 'fun_remove()'); -> 왜 제대로 작동안되는지 알아보기
     remove.addEventListener('click', fun_remove);
     remove.appendChild(remove_Text);
@@ -30,6 +30,7 @@ function add_task(){
 
     let td02 = document.createElement('td');
     td02.innerHTML = task.value;
+    td02.setAttribute('class', 'content');
     tr.appendChild(td02);
 
     let td03 = document.createElement('td');
@@ -49,10 +50,11 @@ function fun_remove(){
 //체크 시 이벤트??
 function fun_checked(){
     let text = this.parentNode.nextSibling;
+    console.log(text);
     if(this.checked){
         text.setAttribute('class', 'checked_task');
     }
     else{
-        text.removeAttribute('class');
+        text.setAttribute('class', 'content');
     }
 }
